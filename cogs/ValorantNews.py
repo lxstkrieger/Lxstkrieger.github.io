760aca14bc74881b007e7008ff5bb54c2072d3f1
Lxstkrieger <80554880+lxstkrieger@users.noreply.github.com>
2024-01-25 13:55:34 +0000

    ValorantNews changed

--- cogs/ValorantNews.py
+++ cogs/ValorantNews.py
@@ -1,56 +1,50 @@
 import discord
-from discord.ext import commands
-from discord.commands import slash_command
-import logging
+from discord.ext import commands, tasks
+import requests
 
 
 class ValorantNews(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
-    
-    API_URL = 'https://vlrggapi.vercel.app/news'
+        self.API_URL = 'https://vlrggapi.vercel.app/news'
 
-
-    async def get_latest_news():
+    async def get_latest_news(self):
         try:
-            response = requests.get(API_URL)
+            response = requests.get(self.API_URL)
             data = response.json()
-            return data["data"]["segments"]
+            return data["data"]["segments"][0]
         except Exception as e:
             print(f"Fehler beim Abrufen der News: {e}")
             return None
 
-    async def send_news_to_channel(news, channel_name):
+    async def send_news_to_channel(self, news, channel_name):
         if news:
-            channel = discord.utils.get(client.guilds[0].channels, name=channel_name)
+            channel = discord.utils.get(self.bot.guilds.channels, name=channel_name)
 
             if channel:
-                for segment in news:
-                    embed = discord.Embed(
-                        title=segment["title"],
-                        description=segment["description"],
-                        color=discord.Color.green()
-                    )
-                    embed.set_author(name=segment["author"])
-                    embed.add_field(name="Datum", value=segment["date"], inline=False)
-                    embed.add_field(name="URL", value=f"[Mehr erfahren]({segment['url_path']})", inline=False)
+                embed = discord.Embed(
+                    title=news["title"],
+                    description=news["description"],
+                    color=discord.Color.magenta()
+                )
+                embed.set_author(name=news["author"])
+                embed.add_field(name="Datum", value=news["date"], inline=False)
+                embed.add_field(name="URL", value=f"[Mehr erfahren]({news['url_path']})", inline=False)
 
-                    await channel.send(embed=embed)
+                await channel.send(embed=embed)
             else:
                 print(f"Kanal '{channel_name}' nicht gefunden.")
 
     @commands.Cog.listener()
-    async def on_ready():
+    async def on_ready(self):
         print(f'Eingeloggt als {self.bot.user.name} ({self.bot.user.id})')
-        news_check.start()
+        self.news_check.start()
 
     @tasks.loop(minutes=30)
-    async def news_check():
-        latest_news = await get_latest_news()
-
-        # Hier kannst du die Kanalnamen anpassen und weitere hinzufügen
+    async def news_check(self):
+        latest_news = await self.get_latest_news()
         for channel_name in ["valorant-news"]:
-            await send_news_to_channel(latest_news, channel_name)
+            await self.send_news_to_channel(latest_news, channel_name)
 
 
 def setup(bot: discord.Bot):
