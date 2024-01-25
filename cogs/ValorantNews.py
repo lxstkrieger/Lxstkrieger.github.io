4c646a7c41bf284dae890f01e98f73c8d70b0240
Lxstkrieger <80554880+lxstkrieger@users.noreply.github.com>
2024-01-25 14:51:46 +0000

    ValorantNews.py are updated

--- cogs/ValorantNews.py
+++ cogs/ValorantNews.py
@@ -8,43 +8,7 @@ class ValorantNews(commands.Cog):
         self.bot = bot
         self.API_URL = 'https://vlrggapi.vercel.app/news'
 
-    async def get_latest_news(self):
-        try:
-            response = requests.get(self.API_URL)
-            data = response.json()
-            return data["data"]["segments"][0]
-        except Exception as e:
-            print(f"Fehler beim Abrufen der News: {e}")
-            return None
-
-    async def send_news_to_channel(self, news, channel_name):
-        if news:
-            channel = discord.utils.get(self.bot.guilds.channels, name=channel_name)
-
-            if channel:
-                embed = discord.Embed(
-                    title=news["title"],
-                    description=news["description"],
-                    color=discord.Color.magenta()
-                )
-                embed.set_author(name=news["author"])
-                embed.add_field(name="Datum", value=news["date"], inline=False)
-                embed.add_field(name="URL", value=f"[Mehr erfahren]({news['url_path']})", inline=False)
-
-                await channel.send(embed=embed)
-            else:
-                print(f"Kanal '{channel_name}' nicht gefunden.")
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f'Eingeloggt als {self.bot.user.name} ({self.bot.user.id})')
-        self.news_check.start()
-
-    @tasks.loop(minutes=30)
-    async def news_check(self):
-        latest_news = await self.get_latest_news()
-        for channel_name in ["valorant-news"]:
-            await self.send_news_to_channel(latest_news, channel_name)
+        # TODO: VALORANT NEWS COMMAND
 
 
 def setup(bot: discord.Bot):
